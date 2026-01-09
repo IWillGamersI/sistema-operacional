df79101b29164f9a3fee65744bb1407cd7c05969
Willian <gameloliniciante@gmail.com>
2026-01-09 11:16:05 +0000

    Atualizações

--- src/app/admin/upload-products/page.tsx
+++ src/app/admin/upload-products/page.tsx
@@ -5,7 +5,7 @@ import { UploadProdutos } from "@/src/components/UploadProducts";
 
 export default function UploadProductsPage() {
   return (
-    <Protected roles={["DEV", "ADMIN"]}>
+    <Protected roles={["DEV", "ADMIN", "OPERACIONAL"]}>
       <UploadProdutos />
     </Protected>
   );
